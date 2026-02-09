d1b54925a681d37d7fe66a76d74e9d25c7b9ff94
g-Aaryan <guptaaaryan1012@gmail.com>
2026-02-09 23:05:10 +0530

    added locks

--- BookingService/src/router/v1/index.router.ts
+++ BookingService/src/router/v1/index.router.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import pingRouter from "./ping.router";
+import bookingRouter from "./booking.router";
 
 const v1router = express.Router();
 
 v1router.use('/ping',pingRouter)
+v1router.use('/bookings', bookingRouter);
 
 export default v1router;
