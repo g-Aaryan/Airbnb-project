2dd26199fd62c813728bcc493919c7ae4ba8e8d6
g-Aaryan <guptaaaryan1012@gmail.com>
2026-05-05 16:36:34 +0530

    added role check middleware

--- AuthInGo/router/role_router.go
+++ AuthInGo/router/role_router.go
@@ -19,6 +19,7 @@ func NewRoleRouter(_roleController *controllers.RoleController) Router {
 
 func (rr *RoleRouter) Register(r chi.Router) {
 	// Role CRUD operations
+	
 	r.Get("/roles/{id}", rr.roleController.GetRoleById)
 	r.Get("/roles", rr.roleController.GetAllRoles)
 	r.With(middlewares.CreateRoleRequestValidator).Post("/roles", rr.roleController.CreateRole)
@@ -30,4 +31,5 @@ func (rr *RoleRouter) Register(r chi.Router) {
 	r.With(middlewares.AssignPermissionRequestValidator).Post("/roles/{id}/permissions", rr.roleController.AssignPermissionToRole)
 	r.With(middlewares.RemovePermissionRequestValidator).Delete("/roles/{id}/permissions", rr.roleController.RemovePermissionFromRole)
 	r.Get("/role-permissions", rr.roleController.GetAllRolePermissions)
+	r.With(middlewares.JWTAuthMiddleware, middlewares.RequireAllRoles("admin")).Post("/roles/{userId}/assign/{roleId}", rr.roleController.AssignRoleToUser)
 }
